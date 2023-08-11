f76c4baae351d72e9bf2e12affcba1f16503f241
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-08-11 17:42:37 +0200

    noise

--- caped_ai_augmentations/AugmentTZYXCsv.py
+++ caped_ai_augmentations/AugmentTZYXCsv.py
@@ -11,9 +11,8 @@ import numpy as np
 from albumentations import transforms    
 from scipy import ndimage
 import pandas as pd
-from numpy.random import normal, poisson
+from photutils.datasets import make_noise_image
 from scipy.ndimage import affine_transform
-from skimage.measure import regionprops
 class AugmentTZYXCsv(object):
 
 
@@ -224,29 +223,41 @@ class AugmentTZYXCsv(object):
       
 
     def _noise_image(self, image, parse_dict):
-        """Add noise of the chosen distribution to all timepoints of the input image."""
-        mean = parse_dict['mean']
-        sigma = parse_dict['sigma']
-        distribution = parse_dict['distribution']   
-        shape = (image.shape[2], image.shape[3])
+          """Add noise of the chosen distribution to all timepoints of the input image."""
+          mean = parse_dict['mean']
+          sigma = parse_dict['sigma']
+          distribution = parse_dict['distribution']   
+          shape = (image.shape[2], image.shape[3])
 
-        if distribution == 'Gaussian':
-            addednoise = normal(loc=mean, scale=sigma, size=shape)
+          if distribution == 'Gaussian':
+                
+                addednoise = make_noise_image(shape, distribution='gaussian', mean=mean,
+                          stddev=sigma)
+              
+          if distribution == 'Poisson':
+  
+                addednoise = make_noise_image(shape, distribution='poisson', mean=sigma)
 
-        elif distribution == 'Poisson':
-            addednoise = poisson(lam=sigma, size=shape)
+          if distribution == 'Both':
 
-        elif distribution == 'Both':
-            gaussiannoise = normal(loc=mean, scale=sigma, size=shape)
-            poissonnoise = poisson(lam=sigma, size=shape)
-            addednoise = gaussiannoise + poissonnoise
+                gaussiannoise = make_noise_image(shape, distribution='gaussian', mean=mean,
+                          stddev=sigma)
+                poissonnoise = make_noise_image(shape, distribution='poisson', mean=sigma)
+            
+                addednoise = gaussiannoise + poissonnoise
 
-        else:
-            raise ValueError('The distribution is not supported. It has to be Gaussian, Poisson, or Both (case-sensitive names)')      
+          else:
 
-        aug_image = image + addednoise[:, :, np.newaxis, np.newaxis]
+            raise ValueError('The distribution is not supported, has to be Gausssian, Poisson or Both (case sensitive names)')      
+          
+          aug_image = image
+          time_points = image.shape[0]
+          z_points = image.shape[1]
+          for i in range(time_points):
+            for j in range(z_points):
+                aug_image[i,j,:,:] =  image[i,j,:,:] + addednoise
 
-        return aug_image
+          return aug_image
 
 
 
